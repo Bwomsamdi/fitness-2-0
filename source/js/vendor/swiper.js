c0687fc5d33d2585a81b116d19bed5d3f5f15321
Bwomsamdi <113728931+Bwomsamdi@users.noreply.github.com>
2023-07-20 03:45:37 +0300

    Добавляет свайпер в проект + добавляет стилистику для блока тренеров

--- source/js/vendor/swiper.js
+++ source/js/vendor/swiper.js
@@ -10175,6 +10175,32 @@ const swiper = () => {
     return Swiper;
   });
   //# sourceMappingURL=swiper-bundle.js.map
+
+  const swiperBlock = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 };
 
 export default swiper();
